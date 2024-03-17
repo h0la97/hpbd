0cedcd3709aefc4c52461ec4845606ef33ec584c
h0la97 <74923318+h0la97@users.noreply.github.com>
2024-03-17 09:38:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+var config = {
+    texts: [
+        "Happy Birthday to the most radiant girl I know! 🎉",      
+        "May your special day be as beautiful and vibrant as you are", 
+        "Here's to a year filled with endless joy, laughter, and unforgettable moments",
+        "You deserve all the love and happiness in the world",
+        "May your dreams take flight and your heart overflow with love and success",
+        "Cheers to another fabulous year ahead! Sending you heaps of hugs, kisses, and warm wishes on your birthday",
+        "Enjoy every moment 🎂🎈"
+    ],
+    imgs: {
+        "Happy Birthday to the most radiant girl I know! 🎉": "./imgs/xiaokeai.png",
+        "May your special day be as beautiful and vibrant as you are": "./imgs/xiaokeai.png",
+        "Here's to a year filled with endless joy, laughter, and unforgettable moments": "./imgs/xiaokeai.png",
+        "You deserve all the love and happiness in the world": "./imgs/xiaokeai.png",
+        "May your dreams take flight and your heart overflow with love and success": "./imgs/xiaokeai.png",
+        "Cheers to another fabulous year ahead! Sending you heaps of hugs, kisses, and warm wishes on your birthday": "./imgs/xiaokeai.png",
+        "Enjoy every moment 🎂🎈": "./imgs/xiaokeai.png"
+    },
+    desc: {
+        turn_on: "Turn On Lights",
+        play: "Play Music",
+        bannar_coming: "Let's Decorate",
+        balloons_flying: "Fly With Balloons",
+        cake_fadein: "Most Delicious Cake Ever",
+        light_candle: "Light Candle",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
